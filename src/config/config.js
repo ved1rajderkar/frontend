4c114804a0a6ee1cd74129c8fbdffc105a0dc757
vedraj <155048436+ved1rajderkar@users.noreply.github.com>
2026-03-14 11:17:57 +0530

    Add files via upload

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,8 @@
 const config = {
-  serverUrl: import.meta.env.VITE_APP_SERVERURL,
+  serverUrl: import.meta.env.VITE_APP_SERVERURL || "/api/v1",
   localUrl: import.meta.env.VITE_APP_LOCALURL || "http://localhost:3030/api/v1",
   proxyUrl: import.meta.env.VITE_APP_PROXYURL,
+  serverUrl2: import.meta.env.VITE_APP_SERVERURL2 || "/api/v1",
 };
 
 export default config;
